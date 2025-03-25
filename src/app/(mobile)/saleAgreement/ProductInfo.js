e38e361e5190aa0f73c57645d2d2d45df59d4223
张三 <shaoqiang_zhang@icloud.com>
2025-03-25 17:15:43 +0800

    feat:增加tab骨架页面

---
+++ src/app/(mobile)/saleAgreement/ProductInfo.js
@@ -0,0 +1,19 @@
+// 一个基本的React组件，接受一个data对象，一个style对象，一个onClick函数，返回一个div元素。
+import React from 'react'
+const ApproveOpinion = () => {
+    return (
+        <div className='p-4'>
+        <div className='mb-4'>
+            4444
+        </div>
+        <div className='mb-4'>
+            111
+        </div>
+        <div className='mb-4'>
+            111
+        </div>
+        
+        </div>
+    )
+}
+export default ApproveOpinion
