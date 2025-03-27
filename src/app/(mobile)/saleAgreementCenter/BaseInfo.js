3efe9a665cbd943cddabea8627aaeaacce3045f1
张三 <shaoqiang_zhang@icloud.com>
2025-03-27 12:24:04 +0800

    feat:   增加字段校对

--- src/app/(mobile)/saleAgreementCenter/BaseInfo.js
+++ src/app/(mobile)/saleAgreementCenter/BaseInfo.js
@@ -11,13 +11,15 @@ import { typeMap } from '@/config/configData'
 import { addCommas } from '@/utils/method'
 import request from '@/utils/request'
 
-import {centerDataInfo,attrData} from './mockData'
+import { centerDataInfo, attrData } from './mockData'
 export default function BaseInfo({ style = {} }) {
+  const usercode = window.localStorage.getItem('acctCode')
+  console.log('用户',usercode)
   const searchParams = useSearchParams()
-  const busKeyValue = searchParams.get('key')||'P1090524080033'
-  const pagCode = searchParams.get('pagCode')||'tp2514'
-  const wfType = typeMap[pagCode].pagCode||'tp2800'
-  const busKey = typeMap[pagCode].busKey||'bilNo'
+  const busKeyValue = searchParams.get('key') || 'P1090524080033'
+  const pagCode = searchParams.get('pagCode') || 'tp2514'
+  const wfType = typeMap[pagCode].pagCode || 'tp2800'
+  const busKey = typeMap[pagCode].busKey || 'bilNo'
 
   const [data, setData] = useState(centerDataInfo)
   const [attaDataList, setAttaDataList] = useState(attrData)
@@ -39,15 +41,19 @@ export default function BaseInfo({ style = {} }) {
   const getBaseInfo = async () => {
     try {
       const result = await request(
-        `/business/mas/tp/manual/${wfType}/getBaseInfo`,
+        `/business/om/auto/bzs_om2120/query/om2100hform`,
         'GET',
         {
-          [busKey]: busKeyValue
+          params:JSON.stringify( { usercode:usercode }),
+          page: 1,
+          start: 0,
+          limit: 9999
         }
       )
+      console.log('basedata', result,  { usercode:usercode })
       if (result && result.success) {
-        setData(result.data || {})
-        const data = result.data || {}
+        setData(result.data[0] || {})
+        const data = result.data[0] || {}
         const params = {
           comCode: data.comCode
         }
@@ -69,72 +75,75 @@ export default function BaseInfo({ style = {} }) {
       {/* {!data ? (
         <Loading />
       ) : ( */}
-        <table className="w-full">
-          <tbody>
-            <tr>
-              <BasicFormItem label="单位" text={data.depName || ''} />
-              <BasicFormItem label="合同号" text={data.perName || ''} />
-            </tr>
-            <tr>
-              <BasicFormItem label="申请日期" text={data.purTitle || ''} />
-              <BasicFormItem label="业务部门" text={data.proName || ''} />
-            </tr>
-            <tr>
-              <BasicFormItem label="销售员" text={data.purTitle || ''} />
-              <BasicFormItem label="合同名称" text={data.proName || ''} />
-            </tr>
-            <tr>
-              <BasicFormItem label="合同类型" text={data.purTitle || ''} />
-              <BasicFormItem label="合同分类" text={data.proName || ''} />
-            </tr>
-            <tr>
-              <BasicFormItem label="业务分类" text={data.purTitle || ''} />
-              <BasicFormItem label="币别名称" text={data.proName || ''} />
-            </tr>
-            <tr>
-              <BasicFormItem label="客户名称" text={data.purTitle || ''} />
-              <BasicFormItem label="客户级别" text={data.proName || ''} />
-            </tr>
-            <tr>
-              <BasicFormItem label="执行地区" text={data.purTitle || ''} />
-              <BasicFormItem label="信息来源" text={data.proName || ''} />
-            </tr>
-            <tr>
-              <BasicFormItem label="签约方式" text={data.purTitle || ''} />
-              <BasicFormItem label="开始日期" text={data.proName || ''} />
-            </tr>
-            <tr>
-              <BasicFormItem label="结束日期" text={data.purTitle || ''} />
-              <BasicFormItem label="合同额" text={data.proName || ''} />
-            </tr>
+      <table className="w-full">
+        <tbody>
+          <tr>
+            <BasicFormItem label="单位" text={data.COM_NAME || ''} />
+            <BasicFormItem label="合同号" text={data.ORD_NO || ''} />
+          </tr>
+          <tr>
+            <BasicFormItem label="申请日期" text={data.SIG_DATE || ''} />
+            <BasicFormItem label="业务部门" text={data.DEP_NAME || ''} />
+          </tr>
+          <tr>
+            <BasicFormItem label="销售员" text={data.SAL_PER_NAME || ''} />
+            <BasicFormItem label="合同名称" text={data.ORD_NAME || ''} />
+          </tr>
+          <tr>
+            <BasicFormItem label="合同类型" text={data.ORD_TYP_NAME || ''} />
+            <BasicFormItem label="合同分类" text={data.CON_CLS_NAME || ''} />
+          </tr>
+          <tr>
+            <BasicFormItem label="业务分类" text={data.BUS_CLS_NAME || ''} />
+            <BasicFormItem label="币别名称" text={data.CUR_NAME || ''} />
+          </tr>
+          <tr>
+            <BasicFormItem label="客户名称" text={data.CUS_NAME || ''} />
+            <BasicFormItem label="客户级别" text={data.LEV_NAME || ''} />
+          </tr>
+          <tr>
+            <BasicFormItem label="执行地区" text={data.CIT_NAME || ''} />
+            <BasicFormItem label="信息来源" text={data.SOU_NAME || ''} />
+          </tr>
+          <tr>
+            <BasicFormItem label="签约方式" text={data.SHI_EXP_FLAG || ''} />
+            <BasicFormItem label="开始日期" text={data.BZS_VAL_DATE || ''} />
+          </tr>
+          <tr>
+            <BasicFormItem label="结束日期" text={data.BZS_INV_DATE || ''} />
+            <BasicFormItem label="合同额" text={data.ORD_TOT_AMT || ''} />
+          </tr>
+          <tr>
+            <BasicFormItem label="税金" text={data.TAX_TOT_AMT || ''} />
+            <BasicFormItem label="不含税金额" text={data.AMT || ''} />
+          </tr>
+          <tr>
+            <BasicFormItem label="SM" text={data.BZS_SM_FLAG || ''} />
+            <BasicFormItem
+              label="是否电子签章"
+              text={data.BZS_OA_DZQ_FLAG || ''}
+            />
+          </tr>
+          <tr>
+            <BasicFormItem label="首付款" text={data.PRE_RET_AMT || ''} />
+            <BasicFormItem
+              label="首付款比例(%)"
+              text={data.PRE_RET_RATE || ''}
+            />
+          </tr>
+          <tr>
+            <BasicFormItem label="是否含运费" text={data.SHI_EXP_FLAG || ''} />
+            <BasicFormItem label="是否终止" text={data.ORD_STATUS || ''} />
+          </tr>
 
-
-
-            <tr>
-              <BasicFormItem label="税金" text={data.purTitle || ''} />
-              <BasicFormItem label="不含税金额" text={data.proName || ''} />
-            </tr>
-            <tr>
-              <BasicFormItem label="SM" text={data.purTitle || ''} />
-              <BasicFormItem label="是否电子签章" text={data.proName || ''} />
-            </tr>
-            <tr>
-              <BasicFormItem label="首付款" text={data.purTitle || ''} />
-              <BasicFormItem label="首付款比例(%)" text={data.proName || ''} />
-            </tr>
-            <tr>
-              <BasicFormItem label="是否含运费" text={data.purTitle || ''} />
-              <BasicFormItem label="是否终止" text={data.proName || ''} />
-            </tr>
-            
-            <tr>
-              <BasicFormItem
-                label="变更原因"
-                text={data.remarks || ''}
-                textColSpan={3}
-              />
-            </tr>
-            {/* <tr>
+          <tr>
+            <BasicFormItem
+              label="变更原因"
+              text={data.MOD_REASON || ''}
+              textColSpan={3}
+            />
+          </tr>
+          {/* <tr>
               <BasicFormItem
                 label="附件"
                 text={() => {
@@ -145,8 +154,8 @@ export default function BaseInfo({ style = {} }) {
                 textColSpan={3}
               />
             </tr> */}
-          </tbody>
-        </table>
+        </tbody>
+      </table>
       {/* )} */}
     </div>
   )
