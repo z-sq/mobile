3fc5df9bf7403a1b1594803d557cf41f283ab4bf
张三 <shaoqiang_zhang@icloud.com>
2025-03-31 17:16:05 +0800

    feat:修改接口文件

--- src/request/apis/saleAgreementCenter.js
+++ src/request/apis/saleAgreementCenter.js
@@ -15,38 +15,26 @@ import { useStores } from '@/utils/useStores'
 // /business/om/auto/om2102/query/tabqddetail 审批页面表格信息
 // /business/om/auto/om2102/crud合同中心审批确认
 
-// /business/om/auto/om_ys_sp/ys_sp_y 合同预算通过接口
-// /business/om/auto/om_ys_sp/ys_sp_r 合同预算不通过接口
-// /business/om/auto/om_cgfk_sp/cgfk_sp_y/ 项目采购付款--通过
-// /business/om/auto/om_cgfk_sp/cgfk_sp_r/ 项目采购付款--不通过
 
-// /business/om/auto/om_hk_sp/om_hk_sp_y/ 回款-通过
-// /business/om/auto/om_hk_sp/om_hk_sp_r/ 回款-不通过
 /**
  * 销售合同接口
  * @type {saleAgreementApi}
  */
 export const saleAgreementApi = {
-  getBaseInfo:'/business/om/auto/bzs_om2120/query/om2100hform',
-  getProInfo: '/business/om/auto/bzs_om2120/query/tabdehead',
+  getBaseInfo:'/business/om/auto/bzs_om2121/query/om2100hform',
+  getProInfo:'/business/om/auto/bzs_om2121/query/tabdehead',
   getCompany: '/business/om/auto/bzs_om2121/query/om2100hform',
   getTable: '/business/om/auto/bzs_om2121/query/tabdehead',
   getCheckDetail: '/business/om/auto/om_qd_cx/query/pm2100qdform',
   getCheckTableDetail: '/business/om/auto/om_qd_cx/query/tabqddetail',
-  getBillhead:'/business/om/auto/om_ys_sp/query/billhead',
-  getBillmgrid:'/business/om/auto/om_ys_sp/query/billmgrid',
-  getPayPlanBase:'/business/om/auto/om_cgfk_sp/query/headformpanel',
-  getPayPlanTable:'/business/om/auto/om_cgfk_sp/query/detailgridpanel',
+  getBillhead:'/business/om/auto/om_hk_sp/query/billform',
+  getBillmgrid:'/business/om/auto/om_ys_cx/query/billmgrid',
+  getPayPlanBase:'/business/om/auto/om_cgfk_cx/query/headformpanel',
+  getPayPlanTable:'/business/om/auto/om_cgfk_cx/query/detailgridpanel',
   getReturnInfo:'/business/om/auto/om_hk_sp/query/billform',
-  getReturnTable:'/business/om/auto/om_hk_sp/query/billform',
+  getReturnTable:'/business/om/auto/om_hk_cx/query/billmgrid',
   getSupplyInfo:'/business/om/auto/om_fz_cx/query/tabfzdetail',
   getApproveBase:'/business/om/auto/om2102/query/pm2102qdform',
   getApproveTable:'/business/om/auto/om2102/query/tabqddetail',
-  getApproveSubmit:'/business/om/auto/om2102/crud',
-  getProductPass:'/business/om/auto/om_ys_sp/ys_sp_y',
-  getProductRefuse:'/business/om/auto/om_ys_sp/ys_sp_r',
-  getPayPass:'/business/om/auto/om_cgfk_sp/cgfk_sp_y/',
-  getPayRefuse:'/business/om/auto/om_cgfk_sp/cgfk_sp_r',
-  getReturnPass:'/business/om/auto/om_hk_sp/om_hk_sp_y',
-  getReturnRefuse:'/business/om/auto/om_hk_sp/om_hk_sp_r'
+  getApproveSubmit:'/business/om/auto/om2102/crud'
 }
